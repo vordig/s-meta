ab4d246c1f5edcde64bbe8ba0d501657b97789cd
Andrey Seregin <vordig@gmail.com>
2023-04-03 00:11:46 +0300

    Initialize Web project and configure Api Versioning and Swagger documentation

--- SMeta.Web/Program.cs
+++ SMeta.Web/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Asp.Versioning;
+using Microsoft.Extensions.Options;
+using SMeta.Web.OpenApi;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+builder.Services.AddSwaggerGen(options => options.OperationFilter<SwaggerDefaultValues>());
+builder.Services.AddApiVersioning(options =>
+{
+    options.ApiVersionReader = new UrlSegmentApiVersionReader();
+}).AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
+
+var app = builder.Build();
+
+var versionSet = app.NewApiVersionSet()
+    .HasApiVersion(new ApiVersion(1.0))
+    .HasApiVersion(new ApiVersion(2.0))
+    .ReportApiVersions()
+    .Build();
+
+var baseApiUrl = "api/v{version:apiVersion}";
+
+app.MapGet($"{baseApiUrl}/hello", () => "Hello world")
+    .WithApiVersionSet(versionSet)
+    .MapToApiVersion(new ApiVersion(1.0));
+
+app.MapGet($"{baseApiUrl}/hello", () => "Hello world v2")
+    .WithApiVersionSet(versionSet)
+    .MapToApiVersion(new ApiVersion(2.0));
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    var descriptions = app.DescribeApiVersions();
+    foreach (var description in descriptions)
+    {
+        var url = $"/swagger/{description.GroupName}/swagger.json";
+        var name = description.GroupName.ToUpperInvariant();
+        options.SwaggerEndpoint(url, name);
+    }
+});
+
+app.Run();
